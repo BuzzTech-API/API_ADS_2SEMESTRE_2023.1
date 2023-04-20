2d716edec6affcf38b0578927629d11b06478bde
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-04-20 16:21:23 -0300

    ajustando painel

--- VAPT/src/GUI/telasInternas/VisualizarAtvEntregueComAtraso.java
+++ VAPT/src/GUI/telasInternas/VisualizarAtvEntregueComAtraso.java
@@ -44,22 +44,25 @@ public class VisualizarAtvEntregueComAtraso extends javax.swing.JInternalFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jLayeredPane1 = new javax.swing.JLayeredPane();
 
-        setPreferredSize(new java.awt.Dimension(220, 264));
+        setPreferredSize(new java.awt.Dimension(828, 275));
 
-        jLayeredPane1.setLayout(new java.awt.GridLayout());
+        jPanel1.setBackground(new java.awt.Color(236, 236, 236));
+
+        jScrollPane1.setBackground(new java.awt.Color(236, 236, 236));
+
+        jLayeredPane1.setBackground(new java.awt.Color(236, 236, 236));
+        jLayeredPane1.setLayout(new java.awt.GridLayout(1, 0));
         jScrollPane1.setViewportView(jLayeredPane1);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 745, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -70,9 +73,7 @@ public class VisualizarAtvEntregueComAtraso extends javax.swing.JInternalFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
